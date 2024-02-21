63eb4c19db117f74561a3f84cd1ae28e6bc88512
Kike Pablos <quique2001@hotmail.com>
2024-02-20 18:38:17 -0600

    res +,-,*

--- calculadora_eq1.R
+++ calculadora_eq1.R
@@ -1,1 +1,43 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Realizar la operación seleccionada
+if (operacion == "+") {
+  resultado <- sumar(num1, num2)
+} else if (operacion == "-") {
+  resultado <- restar(num1, num2)
+} else if (operacion == "*") {
+  resultado <- multiplicar(num1, num2)
