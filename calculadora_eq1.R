5e04c2be02154effb181c01a9cab29dd8392feb8
Diego <a01275561@tec.mx>
2024-02-20 18:54:12 -0600

    Parte_Diego

--- calculadora_eq1.R
+++ calculadora_eq1.R
@@ -1,1 +1,51 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} else if (operacion == "/") {
+  resultado <- dividir(num1, num2)
+} else {
+  resultado <- "Operación no válida"
+}
+
+# Imprimir el resultado
+cat("El resultado de la operación es:", resultado, "\n")
