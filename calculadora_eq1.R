714667edb016e8e7dc8fe52245ca5fcde5e343c9
Avril <delgadoavril18@gmail.com>
2024-02-20 18:46:33 -0600

    CodigoAvril

--- calculadora_eq1.R
+++ calculadora_eq1.R
@@ -1,1 +1,30 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+multiplicar <- function(a, b) {
+  return(a * b)
+}
+
+dividir <- function(a, b) {
+  if (b != 0) {
+    return(a / b)
+  } else {
+    return("Error: No se puede dividir por cero")
+  }
+}
